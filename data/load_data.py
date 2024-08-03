8a7071df99b9c60a81381ecbe960f095a1e53b2e
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2024-08-03 17:12:03 -0300

    Update load_data.py

--- data/load_data.py
+++ data/load_data.py
@@ -1,24 +1,25 @@
-import tensorflow as tf  # Importa la biblioteca TensorFlow y la asigna al alias 'tf'.
-from tensorflow import keras  # Importa el módulo 'keras' desde TensorFlow para construir y entrenar modelos de redes neuronales.
+import tensorflow as tf  # Importamos la biblioteca de TensorFlow y le asignamos el alias 'tf'.
+from tensorflow import keras  # Importamos el módulo 'keras' desde TensorFlow.
 
-
-    fashion_mnist = keras.datasets.fashion_mnist  # Accede al dataset Fashion MNIST desde Keras.
+def load_fashion_mnist_data():  # Definimos la función 'load_fashion_mnist_data' para cargar y procesar los datos del dataset.
+    fashion_mnist = keras.datasets.fashion_mnist  # Accedemos al dataset Fashion MNIST desde Keras.
     
-    # Carga los datos del dataset:
-    # - train_images y train_labels contienen las imágenes y etiquetas para el conjunto de entrenamiento.
-    # - test_images y test_labels contienen las imágenes y etiquetas para el conjunto de prueba.
+    # Cargamos el dataset Fashion MNIST.
+    # El dataset se divide en dos partes:
+    # (train_images, train_labels): Imágenes y etiquetas para el conjunto de entrenamiento.
+    # (test_images, test_labels): Imágenes y etiquetas para el conjunto de prueba.
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  
     
-    # Define los nombres de las clases del dataset Fashion MNIST.
-    # Cada nombre representa una categoría de prenda de vestir en el dataset.
+    # Definimos los nombres de las clases del dataset Fashion MNIST.
+    # Estos nombres representan las categorías de las imágenes en el dataset.
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
     
-    # Normaliza las imágenes del conjunto de entrenamiento.
-    # Los valores de píxeles van de 0 a 255, y dividir por 255.0 convierte estos valores al rango de 0 a 1.
-    # Esto ayuda a que el modelo entrene de manera más eficiente.
+    # Normalizamos las imágenes de entrenamiento.
+    # Dividimos los valores de los píxeles por 255 para que estén en el rango de 0 a 1.
+    # Esto ayuda a que el modelo entrene más eficientemente.
     train_images = train_images / 255.0
     
-    # Normaliza las imágenes del conjunto de prueba de la misma manera que las imágenes de entrenamiento.
-    # Esto asegura que los datos de entrada al modelo durante la evaluación tengan la misma escala.
+    # Normalizamos las imágenes de prueba de la misma manera que las imágenes de entrenamiento.
+    # Esto asegura que los datos de prueba también estén en el rango de 0 a 1.
     test_images = test_images / 255.0
