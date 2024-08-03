219c801ece70444acb36cf5e40a4a2b96c6ec2ec
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2024-08-03 16:10:24 -0300

    Delete load_data.py

--- load_data.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tensorflow import keras
-
-def load_fashion_mnist_data():
-    fashion_mnist = keras.datasets.fashion_mnist
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-    train_images = train_images / 255.0
-    test_images = test_images / 255.0
-    return (train_images, train_labels), (test_images, test_labels)
