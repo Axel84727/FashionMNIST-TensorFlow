d9b23500e2bb0bbd8fd80ef61c1fad83f92c0c1d
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2024-08-03 16:17:36 -0300

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,1 +1,4 @@
-
+def train_model():
+    (train_images, train_labels), (test_images, test_labels) = load_fashion_mnist_data()
+    model = build_model()
+    model.fit(train_images, train_labels, epochs=5)
