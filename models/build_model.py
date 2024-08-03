d8d40ddf4e4389cd0fab21e19150fce42c4a97f5
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2024-08-03 16:16:20 -0300

    Update build_model.py

--- models/build_model.py
+++ models/build_model.py
@@ -1,1 +1,10 @@
-
+def build_model():
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
