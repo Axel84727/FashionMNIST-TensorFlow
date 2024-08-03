1bcb01b08913d179a8691e9fcd82c7600e094855
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2024-08-03 16:19:17 -0300

    Update load_data.py

--- data/load_data.py
+++ data/load_data.py
@@ -4,6 +4,8 @@ from tensorflow import keras
 def load_fashion_mnist_data():
     fashion_mnist = keras.datasets.fashion_mnist
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
     train_images = train_images / 255.0
     test_images = test_images / 255.0
     return (train_images, train_labels), (test_images, test_labels)
